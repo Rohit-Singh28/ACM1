fefc2c15ca6224305d66db2ad13c85906bffdd18
Rohit <rohitkumarkamaldevsingh@gmail.com>
2024-08-23 20:54:12 +0530

    Coming soon page created

---
+++ ACM1/src/Pages/CommingSoon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CommingSoon = () => {
+    
+  return (
+    <div className='bg-zinc-900 h-[100vh] flex items-center justify-center'>
+      <h1 className='text-white font-bold text-5xl md:text-7xl lg:text-[7rem]  p-3'>Coming Soon...</h1>
+    </div>
+  )
+}
+
+export default CommingSoon ;
